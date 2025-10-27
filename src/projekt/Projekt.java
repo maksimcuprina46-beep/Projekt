deab25da7f68f320a38c344c5d4897a843804702
Student <Student@DESKTOP-QA51I19>
2025-10-27 14:55:22 +0300

    были сделаны математические операции с двумя числами

--- src/projekt/Projekt.java
+++ src/projekt/Projekt.java
@@ -14,8 +14,30 @@ public class Projekt {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    public static void main(String[] args) { 
+        // TODO code application logic here 
+        
+        int x=5;
+        int y=9;
+        int sum = x+y;
+        int razn = x-y;
+        int divn = y/x;
+        int mult = x*y;
+        System.out.println("x=5 y=9");
+        System.out.println("sum"+sum);
+        System.out.println("razn"+razn);
+        System.out.println("divn"+divn);
+        System.out.println("mult"+mult);
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }    
     
 }
